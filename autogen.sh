74576db959cbb762780fdc3feadf6778a62c955b
Duncan Roe <duncan_roe@optusnet.com.au>
2021-10-17 12:39:51 +1100

    build: doc: `make` generates requested documentation
    Generate man pages, HTML, neither or both according to ./configure.
    Based on the work done for libnetfilter_queue.
    
    [ This patch updates the default ./configure option to build the
      manpages in case that doxygen is available. ]
    
    Signed-off-by: Duncan Roe <duncan_roe@optusnet.com.au>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,12 @@
 #!/bin/sh -e
 
+BUILD_MAN=doxygen/build_man.sh
+
+# Allow to override build_man.sh url for local testing
+# E.g. export NFQ_URL=file:///usr/src/libnetfilter_queue
+curl ${NFQ_URL:-https://git.netfilter.org/libnetfilter_queue/plain}/$BUILD_MAN\
+  -o$BUILD_MAN
+chmod a+x $BUILD_MAN
+
 autoreconf -fi
 rm -Rf autom4te.cache
